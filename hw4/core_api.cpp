59ed1c720df5e8efc9b49880e0f708dfe9209483
Assafs Local Linux <assafklein@campus.technion.ac.il>
2022-01-13 17:09:29 +0200

    3 test to go

--- hw4/core_api.cpp
+++ hw4/core_api.cpp
@@ -91,17 +91,17 @@ bool Core::executeInst()
 	int src2 = inst.isSrc2Imm ? inst.src2_index_imm : threads[thread].getRegister(inst.src2_index_imm);
 	int stall = 1;
 
-	// std::cout<<cycle<<":\t"<<"["<<thread<<"]\t";
+	//std::cout<<cycle<<":\t"<<"["<<thread<<"]\t";
 
 	switch (inst.opcode)
 	{
 	case CMD_ADDI: 
 	case CMD_ADD: 
 		threads[thread].setRegister(inst.dst_index, src1+src2);
-		// std::cout<<"REG"<<inst.dst_index<<"= "<<src1<<"+"<<src2<<"="<<threads[thread].getRegister(inst.dst_index)<<std::endl;
+		//std::cout<<"REG"<<inst.dst_index<<"= "<<src1<<"+"<<src2<<"="<<threads[thread].getRegister(inst.dst_index)<<std::endl;
 		break;
 	case CMD_HALT: 
-		// std::cout<<"HALT"<<std::endl;
+		//std::cout<<"HALT"<<std::endl;
 		threads[thread].halt();
 		cycle++;
 		inst_count++;
@@ -112,18 +112,21 @@ bool Core::executeInst()
 		SIM_MemDataRead(src1+src2, &value);
 		threads[thread].setRegister(inst.dst_index, value);
 		stall += load_lat;
-		// std::cout<<"REG"<<inst.dst_index<<"= MEM["<<src1<<"+"<<src2<<"]="<<value<<std::endl;
+		//std::cout<<"REG"<<inst.dst_index<<"= MEM["<<src1<<"+"<<src2<<"]="<<value<<std::endl;
+		//std::cout<<" wait until "<<cycle+stall<<std::endl;
 		//std::cout << load_lat << std::endl;
 		break;
 	case CMD_STORE: 
 		SIM_MemDataWrite(dst_val+src2, src1);
 		stall += store_lat;
-		// std::cout<<"MEM["<<dst_val<<"+"<<src2<<"]="<<src2<<std::endl;
+		//std::cout<<"MEM["<<dst_val<<"+"<<src2<<"]="<<src2<<std::endl;
+		//std::cout<<" wait until "<<cycle+stall<<std::endl;
+
 		break;
 	case CMD_SUBI: 
 	case CMD_SUB: 
 		threads[thread].setRegister(inst.dst_index, src1-src2);
-		// std::cout<<"REG"<<inst.dst_index<<"= "<<src1<<"-"<<src2<<"="<<threads[thread].getRegister(inst.dst_index)<<std::endl;
+		//std::cout<<"REG"<<inst.dst_index<<"= "<<src1<<"-"<<src2<<"="<<threads[thread].getRegister(inst.dst_index)<<std::endl;
 		break;
 	
 	default:
@@ -132,19 +135,6 @@ bool Core::executeInst()
 
 	threads[thread].updateStall(cycle+stall);
 	threads[thread].excInst();
-	/*
-	tcontext *blocked = (tcontext*)malloc(SIM_GetThreadsNum() * sizeof(tcontext));
-	CORE_BlockedMT_CTX(blocked, thread);
-	
-	std::cout << "\nRegister file thread id " << thread << ": \n" << std::endl;
-	for (int i=0; i<REGS_COUNT; ++i)
-	{
-	    printf("\tR%d = 0x%X", i, blocked[thread].reg[i]);
-	}
-	
-	std::cout << "\n" << stall << std::endl;
-	free(blocked);
-	*/
 	cycle++;
 	inst_count++;
 	return (stall == 1);
@@ -155,6 +145,25 @@ void Core::contextSwitch(bool latency)
 {
 	//std::cout << "\nthis thread: " << thread << ", numOfCyc: " << cycle << ", numOfInst: " << inst_count << "\n\n\ncontext switch" << std::endl;
 	int next_thread;
+	if (latency)
+	{
+		while(1)
+		{
+			if (!threads[thread].isFinished() && threads[thread].isExecutable(cycle))
+				return;
+			for(unsigned int i = 1; i < threads.size(); i++)
+			{
+				next_thread = (thread + i) % threads.size();
+				if (!threads[next_thread].isFinished() && threads[next_thread].isExecutable(cycle))
+				{
+					thread = next_thread;
+					cycle += switch_lat;
+					return;
+				}
+			}
+			cycle++;
+		}
+	}
 	while(1)
 	{
 		for(unsigned int i = 1; i < threads.size(); i++)
@@ -163,8 +172,6 @@ void Core::contextSwitch(bool latency)
 			if (!threads[next_thread].isFinished() && threads[next_thread].isExecutable(cycle))
 			{
 				thread = next_thread;
-				if(latency)
-					cycle += switch_lat;
 				return;
 			}
 		}
